0fe88bad660fe1b9cbaac0f1e1a5ed260956f8fe
Ikkubaru <iqbal.sp1406@gmail.com>
2024-11-13 14:30:15 +0700

    add javascript file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hello world')
+var fariable = "ini tipe ada string"
+let fariableTwo = 10.1
+let fariableFive = 10.2 // ini number
+const fariableThre = true  // ini boolean
+let fariableFour = 
+[
+    'ini index 0',
+    1,
+    true,
+    'ini index 3',
+    'ini index 4',
+    'ini index 5',
+    'ini index 6',
+] 
+// array
+
+let fariableSix = {
+    id:1, nama:'iqbal'
+}
+let fariableSeven = 
+[
+    {id:1.5, nama:'iqbal', pacar:false},
+    {id:2, nama:'Sholeh'},
+    {id:3, nama:'Prasetyo'},
+    {id:4, nama:'iqbal'},
+]
+
+console.log(fariableSeven)
